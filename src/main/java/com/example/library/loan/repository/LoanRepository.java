8f16faba79b54129c1d9772f97e0b929fbe8845c
GermanMT <germanoctako@gmail.com>
2025-09-23 16:06:20 +0200

    feat: add existsUnreturnedLoanByBookId to loan repository

--- src/main/java/com/example/library/loan/repository/LoanRepository.java
+++ src/main/java/com/example/library/loan/repository/LoanRepository.java
@@ -13,4 +13,9 @@ import com.example.library.loan.model.Loan;
 public interface LoanRepository extends JpaRepository<Loan, Long> {
   @Query("SELECT l FROM Loan l WHERE l.returned = false AND l.user.id = :userId")
   List<Loan> findByUserAndNotReturned(@Param("userId") Long userId);
+
+  @Query("SELECT CASE WHEN COUNT(l) > 0 THEN true ELSE false END " +
+       "FROM Loan l " +
+       "WHERE l.book.id = :bookId AND l.returned = false")
+boolean existsUnreturnedLoanByBookId(@Param("bookId") Long bookId);
 }
